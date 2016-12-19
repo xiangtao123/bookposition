e793c8d3ad1b8ceb04020d4a994d13a9785590b0
xiangtao <xiang_tao123_happy@163.com>
2016-12-19 18:59:44 +0800

    replace html link uri

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -30,27 +30,35 @@ var concatreplace = require('gulp-concat-replace');
 var del = require('del');
 
 
-
-// package path
-var target_path = 'target';
-
 // minify concat
 var dist_path = 'dist';
 
+// src path
+var src_path = 'src';
+
+
 gulp.task('clean', function(){
-  return del([dist_path+'/**/*', target_path+'/**/*']); 
+  return del([dist_path+'/**/*']); 
+});
+
+gulp.task('init',['clean'],function(){
+  return gulp.src([src_path+'/**/*'])
+           .pipe(gulp.dest(dist_path));
+
 });
 
 
 gulp.task('minify-js', function(cb){
-  return gulp.src('js/*.js')
+  return gulp.src(src_path+'/js/*.js')
     .pipe(uglify())
     .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest(dist_path+'/js'));
 });
 
 gulp.task('minify-css', function(){
-  return gulp.src('css/*.css')
+  return gulp.src(src_path+'/css/*.css')
+         .pipe(concatcss('concatenated.css'))
+         .pipe(gulp.dest(dist_path+'/css'))
          .pipe(cleanCSS({debug:true}, function(details){
            console.log(details.name + ': ' + details.stats.originalSize);
            console.log(details.name + ': ' + details.stats.minifiedSize);
@@ -60,47 +68,53 @@ gulp.task('minify-css', function(){
 });
 
 gulp.task('concat-js',['minify-js'], function(){
-  return gulp.src([dist_path+'/**/*.{js,json}'])
+  return gulp.src([dist_path+'/js/*.min.js'])
              .pipe(sourcemaps.init())
              .pipe(concatjs({
-               'target' : 'concatenated.js',
+               'target' : 'concatenated.min.js',
                'entry': './main.js'
-             }))
+              }))
              .pipe(sourcemaps.write())
-             .pipe(gulp.dest(dist_path));
+             .pipe(gulp.dest(dist_path+'/js'));
 });
 
-gulp.task('concat-css',['minify-css'], function(){
-  return gulp.src([dist_path+'/css/*.css'])
+gulp.task('concat-css', function(){
+  return gulp.src([src_path+'/css/*.css'])
     .pipe(concatcss('concatenated.css'))
     .pipe(gulp.dest(dist_path))
 });
 
 
 gulp.task('lint', function(){
-  return gulp.src(['js/*.js'])
+  return gulp.src([src_path+'/js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('minify-replace',['concat-css','concat-js','package'], function(){
-  return gulp.src('*.html')
+gulp.task('concat-replace',['minify-css','concat-js'], function(){
+  return gulp.src(dist_path+'/*.html')
     .pipe(concatreplace({
       prefix:'concat',
       base:'../',
       output:{
-        css:dist_path+'/css',
-        js:dist_path+'/js'
+        css:'css',
+        js:'js'
       }
     }))
-    .pipe(gulp.dest(target_path));// html 替换后的目录
+    .pipe(gulp.dest(dist_path+'/'));// html 替换后的目录
 });
 
-gulp.task('package',['minify-replace'],function(){
-  return gulp.src(['*'])
-           .pipe(gulp.dest(target_path));
+gulp.task('concat-replace-mv',['concat-replace'],function(){
+       gulp.src('js/*')
+         .pipe(gulp.dest(dist_path+'/js/'));
+
+       gulp.src('css/*')
+         .pipe(gulp.dest(dist_path+'/css/'));
+       
+       del('js/*');
+       del('css/*');     
 });
 
-gulp.task('default', ['package']);
+gulp.task('default', ['concat-replace-mv']);
 
 
